6446550179527c48534d6d08d401a0647837c57a
Nah33-max <71524471+Nah33-max@users.noreply.github.com>
2026-02-21 02:43:27 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2117,71 +2117,73 @@ Que vivo está`,
                 tom: 'G',
                 letra: `Intro: ||4/4 [G] | [C]/[G] | [D]/[G] | [C]/[G] [G]/[B] | [C] [D]/[C] | [Bm] [Em] [D] | [C] [G]/[B] [F] | [F] ||
                 
- [G]         [G]/[B] [D]    [D]/[C] [G]/[B]
-Quão formoso és   Rei    do universo
-      [C]   [G]/[B]  [Am]
+ [G]         [G]/[B] [D]  [D]/[C]    [G]/[B]
+Quão formoso és Rei do universo
+      [C]   [G]/[B]     [Am]
 Tua glória enche a terra
-[Am]/[G]       [D]	 
+[Am]/[G]         [D]	 
   E enche os céus
     
      [C]            [D]/[C]
 Tua glória enche a terra
-    [Bm]   [B]/[D#]    [Em]
+    [Bm]    [B]/[D#]     [Em]
 Tua glória enche os céus
-    [Am]               [C]/[D]  |[G] [C]/[G] | [D]/[G] [C]/[G] [C]/[D] | 
+    [Am]                [C]/[D]     |[G] [C]/[G] | [D]/[G] [C]/[G] [C]/[D] | 
 Tua glória enche minha vida Senhor
 
-[G]       [G]/[B] [D] [D]/[C] [G]/[B]
-Quão formoso és   Rei    do universo
-    [C]  [G]/[B]   [Am]
+ [G]         [G]/[B] [D]  [D]/[C]    [G]/[B]
+Quão formoso és Rei do universo
+      [C]   [G]/[B]     [Am]
 Tua glória enche a terra
-[Am]/[G]       [D]	 [G]/[B]
-    E enche os céus
+[Am]/[G]         [D]	 
+  E enche os céus
     
-    [C]          [D]/[C]
+     [C]            [D]/[C]
 Tua glória enche a terra
-    [Bm] [B]/[D#]   [Em]
+    [Bm]    [B]/[D#]     [Em]
 Tua glória enche os céus
-    [Am]             [C]/[D] [G]    [D] 
+    [Am]                [C]/[D]     [G]  [D] 
 Tua glória enche minha vida Senhor
 
-      [G]     [G]/[B]         [C]   [E]
+      [G]       [G]/[B]           [C]   [E]
 Maravilhoso é estar em tua presença
-      [Am]   [Am]/[G]     [D]
+      [Am]     [Am]/[G]       [D]
 Maravilhoso é poder te adorar
-      [Bm]    [B]/[D#]       [Em]
+      [Bm]      [B]/[D#]           [Em]
 Maravilhoso é tocar nas tuas vestes
-     [Am]      [C]/[D]
+      [Am]        [C]/[D]
 Maravilhoso é te contemplar
    [G]	 [D]
 Senhor
-      [G]     [G]/[B]         [C]   [E]
+
+      [G]       [G]/[B]           [C]   [E]
 Maravilhoso é estar em tua presença
-      [Am]   [Am]/[G]     [D]
+      [Am]     [Am]/[G]       [D]
 Maravilhoso é poder te adorar
-      [Bm]    [B]/[D#]       [Em]
+      [Bm]      [B]/[D#]           [Em]
 Maravilhoso é tocar nas tuas vestes
-     [Am]      [C]/[D]
+      [Am]        [C]/[D]
 Maravilhoso é te contemplar
    [G]	 [D]
 Senhor
-      [G]     [G]/[B]         [C]   [E]
+      [G]       [G]/[B]           [C]   [E]
 Maravilhoso é estar em tua presença
-      [Am]   [Am]/[G]     [D]
+      [Am]     [Am]/[G]       [D]
 Maravilhoso é poder te adorar
-      [Bm]    [B]/[D#]       [Em]
+      [Bm]      [B]/[D#]           [Em]
 Maravilhoso é tocar nas tuas vestes
-     [Am]      [C]/[D]
+      [Am]        [C]/[D]
 Maravilhoso é te contemplar
    [G]	 [D]/[E] [Em]/[D] 
 Senhor
-     [Am]      [C]/[D]
+
+      [Am]        [C]/[D]
 Maravilhoso é te contemplar
    [G]	 [D]/[E] [Em]/[D] 
 Senhor
-     [Am]      [C]/[D]
+      [Am]        [C]/[D]
 Maravilhoso é te contemplar
-   [G]	 [C]/[G] [D]/[G] [C]/[G] [G}
+   [G]	 [C]/[G] [D]/[G] [C]/[G] [G]
 Senhor`,
                 favorite: false,
                 createdAt: Date.now() - 172200000
